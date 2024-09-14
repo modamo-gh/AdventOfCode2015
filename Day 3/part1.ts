92ab9246a23207ae51c033cca63b576f2e7edac1
Bryan <morganthemosaic@gmail.com>
2024-09-14 12:59:56 -0400

    Part 1

---
+++ Day 3/part1.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("input.txt", "utf8").trim();
+const currentLocation = { x: 0, y: 0 };
+const visitedHouses = new Set();
+
+visitedHouses.add(JSON.stringify(currentLocation));
+
+for (let i = 0; i < input.length; i++) {
+	switch (input[i]) {
+		case "<":
+			currentLocation.x--;
+			break;
+		case "v":
+			currentLocation.y--;
+			break;
+		case ">":
+			currentLocation.x++;
+			break;
+		case "^":
+			currentLocation.y++;
+			break;
+	}
+
+	visitedHouses.add(JSON.stringify(currentLocation));
+}
+
+console.log(visitedHouses.size);
